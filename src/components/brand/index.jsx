9557c7bcbeb99d6148239d7913c2037f86d6813e
Israfil Hossen <sisrafilss@gmail.com>
2022-02-20 13:07:46 +0600

    brand logo section updated

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
+import "./index.css";
 
 const Brand = ({ data }) => {
     return (
-        <div className="single-brand">
+        <div className="single-brand" style={{ width: "100px !important" }}>
             <img
                 style={{ width: "100px", height: "80px" }}
                 src={process.env.PUBLIC_URL + data.image}
